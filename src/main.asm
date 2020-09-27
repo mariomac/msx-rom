8f339838b12ec3f017f20f0dcc4e2d77b1453612
Mario Macias <mmacias@newrelic.com>
2020-09-27 17:56:49 +0200

    nice screen 2 graph

--- src/main.asm
+++ src/main.asm
@@ -1,8 +1,53 @@
     include "rom/header.asm"
-    include "msx.inc"
+    include "bios.inc"
+    include "graficos.asm"
 
 main:
-    call BIOS_BEEP
-    jp main
+    ; set colors
+    ld A, COL_WHITE
+    ld (ADDR_FORCLR), A
+    ld A, COL_TRANSPARENT
+    ld (ADDR_BAKCLR), A
+    ld (ADDR_BDRCLR), A
+    call BIOS_CHGCLR
+
+    ; set screen 2
+    call BIOS_INIGRP
+
+    ; load data into vram. Triplicating tiles and color definitions
+    ; for screen 2
+    ld hl, BANK_PATTERN_0
+    ld de, SCR2_CHARPATTERN
+    ld bc, 24
+    call BIOS_LDIRVM
+    ld hl, BANK_PATTERN_0
+    ld de, SCR2_CHARPATTERN + 8*32*8
+    ld bc, 24
+    call BIOS_LDIRVM
+    ld hl, BANK_PATTERN_0
+    ld de, SCR2_CHARPATTERN + 16*32*8
+    ld bc, 24
+    call BIOS_LDIRVM    
+
+    ld hl, BANK_COLOR_0
+    ld de, SCR2_PIXELCOLOR
+    ld bc, 24
+    call BIOS_LDIRVM    
+    ld hl, BANK_COLOR_0
+    ld de, SCR2_PIXELCOLOR + 8*32*8
+    ld bc, 24
+    call BIOS_LDIRVM    
+    ld hl, BANK_COLOR_0
+    ld de, SCR2_PIXELCOLOR + 16*32*8
+    ld bc, 24
+    call BIOS_LDIRVM    
+
+    ld hl, SCREEN_MAP
+    ld de, SCR2_CHARPOS
+    ld bc, 32*24
+    call BIOS_LDIRVM
+
+loop:
+    jp loop
 
     include "rom/tail.asm"
