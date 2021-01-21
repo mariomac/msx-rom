02536810aa29c9ab2c68a3d88ab78c43cce8f420
Mario Macias <mmacias@newrelic.com>
2021-01-21 16:56:13 +0100

    added sjasmplus license

---
+++ src/assets/sprites.asm
@@ -0,0 +1,36 @@
+AMANCIO:
+    ; patterns 0-3
+    DB $0F,$1F,$3F,$19,$1D,$1F,$3D,$1D
+    DB $01,$00,$00,$00,$00,$E0,$60,$00
+    DB $F0,$F8,$FC,$98,$B8,$F8,$BC,$BC
+    DB $80,$00,$00,$00,$00,$07,$06,$00
+    ; patterns 4-7
+    DB $00,$00,$40,$E0,$E0,$E0,$40,$00
+    DB $60,$FE,$EF,$EF,$1F,$0E,$0E,$1F
+    DB $00,$00,$02,$07,$07,$07,$02,$00
+    DB $02,$7F,$F7,$F7,$F8,$F0,$F0,$78
+
+
+AMANCIO_END:
+
+/* backup:
+!type
+msx1
+#Slot 0
+....99999999....
+...9999999999...
+.E999999999999E.
+EEE99..99..99EEE
+EEE999.99.999EEE
+EEE9999999999EEE
+.E9999.99.9999E.
+...999.99.9999..
+.EE....99.....E.
+EEEEEEE..EEEEEEE
+EEE.EEEEEEEE.EEE
+EEE.EEEEEEEE.EEE
+...EEEEEEEEEE...
+999.EEE.EEEE.999
+.99.EEE.EEEE.99.
+...EEEEE.EEEE...
+*/
